fb54906390157e10d427a23fbf3bd6ea7e8a0b1e
seanmcalinden <sean.mcalinden@amido.co.uk>
2013-02-19 23:01:43 +0000

    Created Shell Project with Initial Dock Panels Setup

---
+++ RestBox/RestBox/Bootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using Microsoft.Practices.ServiceLocation;
+using PrismContrib.WindsorExtensions;
+using RestBox.Installers;
+
+namespace RestBox
+{
+    public class Bootstrapper : WindsorBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            Container.Install(new ShellInstaller());
+
+            var serviceLocator = ServiceLocator.Current;
+
+            var instance = serviceLocator.GetInstance<Shell>();
+            return instance;
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow = (Window)Shell;
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
---
+++ RestBox/RestBox/Installers/ShellInstaller.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace RestBox.Installers
+{
+    public class ShellInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(AllTypes
+                                 .FromThisAssembly()
+                                 .BasedOn<Window>()
+                                 .Configure(c => c.LifeStyle.Singleton.Named(c.Implementation.Name)));
+        }
+    }
+}
